da4cb28c1f43a9ce9a3fae7f19ccca15218bac74
Patrick Hare <patrickhare@Patricks-MacBook-Air.local>
2020-05-22 11:21:51 +0100

    adding days 3-5

---
+++ Day4.py
@@ -0,0 +1,40 @@
+# This problem was asked by Stripe.
+
+# Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+
+# For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+# You can modify the input array in-place.
+
+# not linear time - O(log(n) + n)
+def return_missing_int(input_list):
+    input_list.sort()
+    for index, i in enumerate(input_list):
+        if i >= 0:
+            if input_list[index+1] == i+1:
+                return
+            else:
+                return i
+
+# given answer
+def first_missing_positive(nums):
+    if not nums:
+        return 1
+    for i, num in enumerate(nums):
+        while i + 1 != nums[i] and 0 < nums[i] <= len(nums):
+            v = nums[i]
+            nums[i], nums[v - 1] = nums[v - 1], nums[i]
+            if nums[i] == nums[v - 1]:
+                break
+    for i, num in enumerate(nums, 1):
+        if num != i:
+            return i
+    return len(nums) + 1
+
+# nice answer but not linear space complexity 
+def first_missing_positive(nums):
+    s = set(nums)
+    i = 1
+    while i in s:
+        i += 1
+    return i
